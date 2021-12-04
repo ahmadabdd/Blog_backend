06c065461472fcbdb3bdb69410e082d4617ecd77
Ahmad Abd <58667082+ahmadabdd@users.noreply.github.com>
2021-12-04 21:26:04 +0200

    Created Signup and Login APIs

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const postRoute = require('./routes/posts');
+const userRoute = require('./routes/user');
 
 const app = express();
-
-const postRoute = require('./routes/posts');
 
 app.use(bodyParser.json());
 
 app.use("/posts", postRoute); 
+app.use("/user", userRoute); 
 
 module.exports = app;
